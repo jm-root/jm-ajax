0f16bd3193f1c5e7dd6494deac2f89577e90d3f2
jeff yu <jeff@jamma.cn>
2017-01-15 19:40:16 +0800

    Suport headers

--- tests/ajax.js
+++ tests/ajax.js
@@ -6,9 +6,15 @@ if (typeof module !== 'undefined' && module.exports) {
 (function(){
     var logger = jm.logger;
     var obj = {};
-    jm.enableAjax(obj, {timeout: 1000});
+    jm.enableAjax(obj, {timeout: 5000});
     obj.get({
-        url: 'http://localhost:20100/sso/issignon'
+        url: 'http://localhost:3000/users/123',
+        data: {
+            name: 123
+        },
+        headers: {
+            'X-Forwarded-For': '128.0.0.1, 129.0.0.1'
+        }
     }, function(err, doc){
         if(err) logger.error(err.stack);
         if(doc) logger.debug(JSON.stringify(doc));
